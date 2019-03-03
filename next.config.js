2e028c351540d2327aef8eb33de6f79bd985d9f9
Taran Pierce <taran.pierce@endurance.com>
2019-03-03 13:46:09 -0700

    feature/next-js
    working on export setup

---
+++ next.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  exportPathMap: function () {
+    return {
+      '/': { page: '/' },
+      '/better-body': { page: '/better-body' },
+      '/advanced-conditioning': { page: '/advanced-conditioning' },
+      '/body-building': { page: '/body-building' },
+      '/strength-training': { page: '/strength-training' },
+      '/aerobic': { page: '/aerobic' },
+      '/upper-lower-body': { page: '/upper-lower-body' },
+      '/anaerobic': { page: '/anaerobic' }
+    }
+  }
+}
