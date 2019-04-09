43d4aab340fd7bc43bf0d9398761974b867baddf
Taran Pierce <taran.pierce@endurance.com>
2019-04-08 20:57:11 -0600

    feature/setup-production-build
    updated production build

--- next.config.js
+++ next.config.js
@@ -2,13 +2,13 @@ module.exports = {
   exportPathMap: function () {
     return {
       '/': { page: '/' },
-      '/better-body': { page: '/better-body' },
-      '/advanced-conditioning': { page: '/advanced-conditioning' },
-      '/body-building': { page: '/body-building' },
-      '/strength-training': { page: '/strength-training' },
-      '/aerobic': { page: '/aerobic' },
-      '/upper-lower-body': { page: '/upper-lower-body' },
-      '/anaerobic': { page: '/anaerobic' }
+      '/better-body': { page: '/workout', query: { title: 'better-body'} },
+      '/advanced-conditioning': { page: '/workout', query: { title: 'advanced-conditioning'} },
+      '/aerobic': { page: '/workout', query: { title: 'aerobic'} },
+      '/anaerobic': { page: '/workout', query: { title: 'anaerobic'} },
+      '/body-building': { page: '/workout', query: { title: 'body-building'} },
+      '/strength-training': { page: '/workout', query: { title: 'strength-training'} },
+      '/upper-lower-body': { page: '/workout', query: { title: 'upper-lower-body'} },
     }
   }
 }
